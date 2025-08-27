ab4f0b2e650d55d9b6efe3099e012ec60e394bab
DJAngel973 <angel.djuancho@gmail.com>
2025-08-27 18:22:59 -0500

    feat: Methods are create to create orders, search for customers, search for orders by ID, search for all orders by customer, and display orders and customers in general.

--- src/main/java/co/edu/poli/service/orderSystem.java
+++ src/main/java/co/edu/poli/service/orderSystem.java
@@ -1,11 +1,17 @@
 package co.edu.poli.service;
 
+import co.edu.poli.exception.ClientNotFoundException;
 import co.edu.poli.exception.IdDuplicadoException;
 import co.edu.poli.exception.InvalidDataException;
+import co.edu.poli.exception.OrderNotFoundException;
 import co.edu.poli.model.Client;
 import co.edu.poli.model.Order;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class orderSystem {
     private Map<String, Client> clients;
@@ -18,8 +24,6 @@ public class orderSystem {
 
     /**
      * Registra un nuevo cliente en el sistema.
-     * Lanza DatosInvalidosException si los datos del cliente no son válidos.
-     * Lanza IdDuplicadoException si el ID del cliente ya existe.
      */
     public void registerClient(Client client){
         if(client == null || client.getId() == null || client.getId().trim().isEmpty() || client.getName() == null ||
@@ -32,5 +36,100 @@ public class orderSystem {
         clients.put(client.getId(), client);
         System.out.printf("Cliente registrado exitosamente: %s (Id: %s)",client.getName(), client.getId());
     }
-    
+
+    /**
+     * Crea un nuevo pedido para un cliente existente.
+     * */
+    public Order createOrder(String clientId, Double total, List<String> details) throws ClientNotFoundException {
+        if (clientId == null || clientId.trim().isEmpty() || total <= 0 || details == null || details.isEmpty()) {
+            throw new InvalidDataException("Datos del pedido incompletos o inválidos.");
+        }
+        if (!clients.containsKey(clientId)){
+            throw new ClientNotFoundException(String.format("El cliente con ID %s no existe", clientId));
+        }
+        String orderId = UUID.randomUUID().toString(); // Genera un ID único para el pedido.
+        if (orders.containsKey(orderId)) { // Buena práctica verificar.
+            throw new IdDuplicadoException("ID del pedido generado duplicado. Intente de nuevo.");
+        }
+        Order newOrder = new Order(orderId, clientId, LocalDate.now(),total, details);
+        orders.put(orderId, newOrder);
+        System.out.printf("Pedido %s creado exitosamente para el cliente %s.", newOrder.getId(), clientId);
+        return newOrder;
+    }
+
+    /**
+     * Buscar un cliente por su ID.
+     * Search a client for ID.
+     * */
+    public Client searchClient(String id) throws ClientNotFoundException {
+        if (id == null || id.trim().isEmpty()) {
+            throw new InvalidDataException("El ID del cliente no puede ser nulo o vacío.");
+        }
+        Client client = clients.get(id);
+        if (client == null) {
+            throw new ClientNotFoundException(String.format("Cliente con ID %s no encontrado.",id));
+        }
+        return client;
+    }
+
+    /**
+     * Buscar un pedido por su ID.
+     * Search order for ID
+     * */
+    public Order searchOrder(String id) throws OrderNotFoundException {
+        if (id == null || id.trim().isEmpty()) {
+            throw new OrderNotFoundException("El ID del pedido no puede ser nulo o vacío.");
+        }
+        Order order = orders.get(id);
+        if (order == null) {
+            throw new OrderNotFoundException(String.format("El pedido con ID %s no encontrado.", id));
+        }
+        return order;
+    }
+
+    /**
+     * Buscar todos los pedidos realizados por un cliente en específico.
+     * Search all orders for a client.
+     * */
+    public List<Order> searchOrdersClient(String clientId) throws ClientNotFoundException {
+        if (clientId == null || clientId.trim().isEmpty()) {
+            throw new ClientNotFoundException("El ID del cliente no puede ser nulo o vacío.");
+        }
+        if (!clients.containsKey(clientId)) {
+            throw new ClientNotFoundException(String.format("El cliente con ID %s no existe.", clientId));
+        }
+        List<Order> ordersClient = new ArrayList<>();
+        for (Order order : orders.values()) {
+            if (order.getClientId().equals(clientId)){
+                ordersClient.add(order);
+            }
+        }
+        return ordersClient;
+    }
+
+    /**
+     * Listar todos los clientes.
+     * */
+    public void listAllClients() {
+        if (clients.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+            return;
+        }
+        System.out.println("\n------ Clientes registrados ------");
+        clients.values().forEach(System.out::println);
+        System.out.println("----------------------------------");
+    }
+
+    /**
+     * Listar todos los pedidos.
+     * */
+    public void listAllOrders(){
+        if (orders.isEmpty()) {
+            System.out.println("No hay pedidos registrados.");
+            return;
+        }
+        System.out.println("\n------ Pedidos registrados ------");
+        orders.values().forEach(System.out::println);
+        System.out.println("-----------------------------------");
+    }
 }
