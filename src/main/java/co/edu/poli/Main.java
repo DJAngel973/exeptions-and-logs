c13537a1cfef3fcbb7c1d44d849db12feb138949
DJAngel973 <angel.djuancho@gmail.com>
2025-08-27 23:59:35 -0500

    feat: main is added to start

--- src/main/java/co/edu/poli/Main.java
+++ src/main/java/co/edu/poli/Main.java
@@ -1,17 +1,22 @@
 package co.edu.poli;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import co.edu.poli.service.OrderService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Main class of the Spring Boot application.
+ * <p>
+ * The {@code @SpringBootApplication} annotation is a shortcut for {@code @Configuration},
+ * {@code @EnableAutoConfiguration}, and {@code @ComponentScan} with their default attributes.
+ * Its sole responsibility is to bootstrap the Spring application.
+ * </p>
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        // La responsabilidad de la clase Main es solo arrancar la aplicación Spring.
+        SpringApplication.run(Main.class, args);
     }
 }
