e7191ef449e11c249bef4da702f99aa2082b6bec
DJAngel973 <angel.djuancho@gmail.com>
2025-09-01 23:18:10 -0500

    docs: add documentation explaining class type, methods with their constructor

--- src/main/java/co/edu/poli/model/Client.java
+++ src/main/java/co/edu/poli/model/Client.java
@@ -2,11 +2,23 @@ package co.edu.poli.model;
 
 import java.util.Objects;
 
+/**
+ * Represents a customer's information in the management system.
+ * This class stores a customer's basic data, such as their unique identifier,
+ * their name, and their email address.
+ * */
 public class Client {
     private String id;
     private String name;
     private String email;
 
+    /**
+     * Constructor to create a new instance of {@code Client}.
+     *
+     * @param id The unique identifier of the client.
+     * @param name The full name of the client.
+     * @param email The email address of the client.
+     * */
     public Client(String id, String name, String email){
         this.id = id;
         this.name = name;
@@ -23,6 +35,13 @@ public class Client {
         return id;
     }
 
+    /**
+     * Compares this client object to another client object to verify equality.
+     * Equality is determined based solely on the client's unique identifier.
+     *
+     * @param one The object to compare with this client.
+     * @return {@code true} if the objects are the same or have the same ID, {@code false} otherwise.
+     * */
     @Override
     public boolean equals(Object one){
         if (this == one) return true;
@@ -31,11 +50,12 @@ public class Client {
         return Objects.equals(id, client.id);
     }
 
-    //@Override
-    //public int hasCode(){
-    //    return Objects.hash(id);
-    //}
-
+    /**
+     * Returns a string representation of the customer's information.
+     * The format is a multi-line string that includes the customer's ID, name, and email address.
+     *
+     * @return A string representation of the customer.
+     * */
     @Override
     public String toString(){
         return String.format("""
