764c7500152399f8afe2f004a212ae267914e772
DJAngel973 <angel.djuancho@gmail.com>
2025-10-11 18:55:43 -0500

    feat: add DTO orderResponde user

--- src/main/java/co/edu/JdA/DTO/OrderResponseDTO.java
+++ src/main/java/co/edu/JdA/DTO/OrderResponseDTO.java
@@ -1,4 +1,18 @@
 package co.edu.JdA.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponseDTO {
+    private String id;
+    private LocalDate orderDate;
+    private Double total;
+    private List<String> details;
 }
