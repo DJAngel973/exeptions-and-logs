f1056b3c3453d7d51db399ef148f5621fc6996f2
DJAngel973 <angel.djuancho@gmail.com>
2025-10-11 20:00:43 -0500

    refactor: organizes DTO for Single Responsibility

--- src/main/java/co/edu/JdA/DTO/OrderResponseDTO.java
+++ src/main/java/co/edu/JdA/DTO/OrderResponseDTO.java
@@ -1,18 +1,45 @@
 package co.edu.JdA.DTO;
 
+import co.edu.JdA.entity.OrderEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Data Transfer Object (DTO) for order-related responses.
+ * This class encapsulates the data to be returned to the client
+ * after a successful operation, providing a clean representation
+ * of the order's information.
+ * */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderResponseDTO {
     private String id;
+    private String clientId;
     private LocalDate orderDate;
     private Double total;
     private List<String> details;
+
+    /**
+     * Converts an {@link OrderEntity} object into an {@code OrderResponseDTO}.
+     * This method acts as a factory method to map the entity to its corresponding DTO.
+     * @param entity The {@link OrderEntity} to be converted
+     * @return An {@code OrderResponseDTO} containing the mapped data.
+     * */
+    public static OrderResponseDTO fromEntity(OrderEntity entity) {
+        List<String> detailsList = entity.getDetails() != null && !entity.getDetails().isEmpty()
+                ? Array.asList(entity.getDetails().split(", "))
+                : List.of();
+        return new OrderResponseDTO(
+                entity.getId(),
+                entity.getClient().getId(),
+                entity.getOrderDate(),
+                entity.getTotal(),
+                detailsList
+        );
+    }
 }
